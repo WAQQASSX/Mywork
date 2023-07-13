d1a3b77d28c8ac9619f9a21598e7311546fdedf0
walsadoo <waqqas5000@gmail.com>
2023-07-13 08:30:59 -0400

    first commit

--- bill.py
+++
@@ -1,8 +0,0 @@
-ItPrice = float( input("the price of the prodact "))
-Tax = float(input("ratio of tax "))
-tip = int(input("ratio of the tip "))
-
-bill1 = ItPrice + ((Tax/100)*ItPrice) + ((tip/100.0)*ItPrice) 
-bill2  = bill1/2 
-print(bill1)
-print(bill2)
--- main.py
+++
@@ -1,7 +0,0 @@
-name ="waqqas" 
-age = "19"
-salary = "30"
-
-list1 ='my name is '+ name + ' my age is : '+ age + ' my salary is  ' + salary + ' bd  :(' 
-
-print(list1)
