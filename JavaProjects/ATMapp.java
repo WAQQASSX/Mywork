d288405ce604652ee72e59f30c495b96eb76a6af
WAQQASSX <waqqas5000@gmail.com>
2023-09-10 08:47:18 +0300

    d

--- JavaProjects/ATMapp.java
+++ JavaProjects/ATMapp.java
@@ -1,12 +1,11 @@
 package JavaProjects;
-
 import javax.swing.JOptionPane;
 public class ATMapp{
 public static void main(String[] args) {
     JOptionPane.showMessageDialog(null,"Welcome in WS BANK");
     String LorR =  JOptionPane.showInputDialog("Please do you gave account or not (y/n) ");
     if (LorR.equals("y")){
-        login olduser = new login();
+        login.login();
 
     }else if(LorR.equals("n")){
        JOptionPane.showMessageDialog(null,"Please go to the agency and Create Account  ");
@@ -24,24 +23,24 @@ class database {
 
 }
 class login{
+static void login(){
     database data = new database();
-login(){
     String clint = JOptionPane.showInputDialog("Please Enter your UserName ");
     String clintpass = JOptionPane.showInputDialog("Please Enter your Password ");
     if (clint.equals(data.User1) &&  clintpass.equals(data.password1)){
         int choice = Integer.parseInt(JOptionPane.showInputDialog("Please Choice what you want to do 1 :Dopiest 2:Withdraw 3:statement"));
         switch (choice) {
             case 1:
-                dopiest addmoney = new dopiest();
+                dopiest.dopiest();
 
                 break;
         
             case 2:
-            withdraw withdrawMony = new withdraw();
+            withdraw.withdraw();
 
                 break;
             case 3 :
-                statement shm = new statement();
+                statement.statement();
                 break;
             default:
             JOptionPane.showMessageDialog(null, "Invalid data");
@@ -55,50 +54,51 @@ login(){
 }
 
 class dopiest {
- database data = new database();
-dopiest(){
+static void dopiest(){
+     database data = new database();
     int addMoney = Integer.parseInt(JOptionPane.showInputDialog("How Much Money You want to add ?"));
    data.credit1 += addMoney ; 
     JOptionPane.showMessageDialog(null, "you have this amount of money = " +data.credit1);
-     anotPros n = new anotPros();
+     anotPros.anotPros();
 }
 
 }
 class withdraw{
-     database data = new database();
- withdraw(){
+ static void withdraw(){
+    database data = new database();
 int submoney = Integer.parseInt(JOptionPane.showInputDialog("How Much Money You want  ?"));
     data.credit1 -= submoney ; 
     JOptionPane.showMessageDialog(null, "Please take you money " );
     JOptionPane.showMessageDialog(null, "you have this amount of money = " + data.credit1);
-     anotPros n = new anotPros();
+     anotPros.anotPros();
  }
 } 
 class statement{     
   static void statement(){
         database data = new database();
         JOptionPane.showMessageDialog(null, " your Credit is : " + data.credit1);
-        anotPros n = new anotPros();
+       anotPros.anotPros();
 }
 }
 class anotPros {
-     database data = new database();
-    anotPros(){
+    static void anotPros(){
+         database data = new database();
         String YorN = JOptionPane.showInputDialog("Do you need anything else  ?? (y/n)");
         if (YorN.equals("y")){
             int choice = Integer.parseInt(JOptionPane.showInputDialog("Please Choice what you want to do 1 :Dopiest 2:Withdraw 3:statement"));
         switch (choice) {
             case 1:
-                dopiest addmoney = new dopiest();
+                dopiest.dopiest();
 
                 break;
         
             case 2:
-            withdraw withdrawMony = new withdraw();
+            withdraw.withdraw();
+
 
                 break;
             case 3 :
-                statement shm = new statement();
+                statement.statement();
                 break;
         }
         }else if(YorN.equals("n")){
